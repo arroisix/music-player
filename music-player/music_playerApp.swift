618dde1bff20a80dae8915f69ef6c3f30c25f0a8
arroisix <arroro99@gmail.com>
2026-02-03 10:13:56 +0700

    Initial Commit

---
+++ music-player/music_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_playerApp.swift
+//  music-player
+//
+//  Created by Satrio Indrajit Arroisi on 03/02/26.
+//
+
+import SwiftUI
+
+@main
+struct music_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
